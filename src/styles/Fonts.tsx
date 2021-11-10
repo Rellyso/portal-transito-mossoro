f1778cd8b2bc8415cead9d971a1e2917c6be9e81
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-11-10 07:12:38 -0300

    added custom font styles

---
+++ src/styles/Fonts.tsx
@@ -0,0 +1,25 @@
+import { Global } from "@emotion/react";
+
+const Fonts = () => (
+  <Global
+    styles={`
+      @font-face {
+        font-family: "PantonBlackCaps";
+        font-style: normal;
+        font-weight: 700;
+        font-display: swap;
+        src: url("../fonts/PantonBlackCaps.otf") format('otf');
+      }
+
+      @font-face {
+        font-family: "Rede Rounded F31";
+        font-style: normal;
+        font-weight: 700;
+        font-display: swap;
+        src: url("../fonts/Rede\ Rounded\ F31.ttf") format('ttf');
+      }
+    `}
+  />
+)
+
+export default Fonts;
