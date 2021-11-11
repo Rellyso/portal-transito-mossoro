28d7ed059396b1904b68d0e8f2e4675759479119
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-11-11 07:20:13 -0300

    ➕ added services bar component

---
+++ src/components/ServicesBar/index.tsx
@@ -0,0 +1,28 @@
+import { Image } from "@chakra-ui/image";
+import { Box, Flex, Link } from "@chakra-ui/layout";
+
+import styles from './styles.module.scss';
+
+export function ServicesBar() {
+  return (
+    <Flex
+      className={styles.services_bar}
+      w="100%"
+      bgColor="white"
+      px="8"
+      align="center"
+      justify="space-between"
+      boxShadow="0 0 10px 6px rgba(0,0,0,0.15)"
+      zIndex="20"
+    >
+      <Image
+        src="/images/mossoro-logo.png"
+        h="12"
+      />
+
+      <Box>
+        <Link href="#" color="blue.700" fontWeight="medium">Serviços</Link>
+      </Box>
+    </Flex>
+  )
+}
