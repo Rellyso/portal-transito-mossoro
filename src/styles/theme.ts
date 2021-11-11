ced72f2f402e53d8b9f5f3269ae189ae93b76a3d
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-11-11 07:34:26 -0300

    🆙 updated global styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,8 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
+  initialColorMode: 'light',
+  useSystemColorMode: false,
   fonts: {
     heading: 'Poppins',
     body: 'Roboto'
