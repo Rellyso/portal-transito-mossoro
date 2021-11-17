0106497002876191c7c06ad560f8f8f72747c1ed
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-11-17 10:31:54 -0300

    Created dropdown menu component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,34 @@
+import Icon from "@chakra-ui/icon";
+import { Menu, MenuButton, MenuItem, MenuList, MenuProps } from "@chakra-ui/menu";
+import { Link } from "../Content/Link";
+
+import { ChevronDownIcon } from '@chakra-ui/icons';
+
+import styles from './styles.module.scss'
+import { HStack } from "@chakra-ui/layout";
+
+type Props = {
+  title: string,
+  items: {
+    name: string;
+    href: string;
+  }[]
+}
+
+export function Dropdown({ title, items }: Props) {
+  return (
+    <Menu isLazy >
+      <MenuButton rightIcon={<ChevronDownIcon color="blue.700" />} color="blue.700" fontFamily="Poppins" fontWeight="600">{title}</MenuButton>
+      <MenuList>
+        {/* MenuItems are not rendered unless Menu is open */}
+        {items.map(item => (
+          <Link href={item.href} key={item.name}>
+            <MenuItem>
+              {item.name}
+            </MenuItem>
+          </Link>
+        ))}
+      </MenuList>
+    </Menu>
+  )
+}
