f4f9e099fd0c89fa004c81a181936e5cbc01c4aa
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-11-18 19:50:59 -0300

    🖌️ got styled scrollbar

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,5 +6,19 @@ export const theme = extendTheme({
   fonts: {
     heading: 'Poppins',
     body: 'Roboto'
+  },
+  styles: {
+    global: {
+      '&::-webkit-scrollbar': {
+        width: '8px',
+      },
+      '&::-webkit-scrollbar-track': {
+        width: '8px',
+      },
+      '&::-webkit-scrollbar-thumb': {
+        background: '#00ADEF',
+        borderRadius: '24px',
+      },
+    }
   }
 })
